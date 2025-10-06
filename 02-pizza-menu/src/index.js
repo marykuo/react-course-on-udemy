45c5dc7071d8d84be57e2db2b07f6799d7eb49cf
marykuo <meiyu19980928@gmail.com>
2025-10-06 22:11:32 +0800

    feat: styling react application

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
@@ -12,16 +13,19 @@ function App() {
 }
 
 function Header() {
+  // example of inline style
+  // const style = { color: "red", fontSize: "50px" };
+  const style = {};
   return (
-    <header>
-      <h1>Fast React Pizza Co.</h1>
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
     </header>
   );
 }
 
 function Menu() {
   return (
-    <menu>
+    <menu className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
@@ -31,7 +35,7 @@ function Menu() {
 
 function Pizza() {
   return (
-    <div>
+    <div className="pizza">
       <img src="pizzas/focaccia.jpg" alt="Focaccia" />
       <h3>Focaccia</h3>
       <p>Bread with italian olive oil and rosemary</p>
@@ -40,7 +44,7 @@ function Pizza() {
 }
 
 function Footer() {
-  return <footer>We're currently open!</footer>;
+  return <footer className="footer">We're currently open!</footer>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
