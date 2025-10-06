4da9e3b11ff30fdfe18cb7c096f0fe9ffb307268
marykuo <meiyu19980928@gmail.com>
2025-10-06 21:34:50 +0800

    feat: Header, Menu, Footer components

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -2,7 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <menu>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+    </menu>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="Focaccia" />
+      <h3>Focaccia</h3>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>We're currently open!</footer>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
