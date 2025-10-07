10c43976b09765c742da377864e493b4dd5fd59c
marykuo <meiyu19980928@gmail.com>
2025-10-07 22:12:15 +0800

    feat: show order button only when store is open

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -51,7 +51,29 @@ function Pizza(props) {
 }
 
 function Footer() {
-  return <footer className="footer">We're currently open!</footer>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open from {openHour}:00 to {closeHour}:00. Come visit us or
+            order online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
