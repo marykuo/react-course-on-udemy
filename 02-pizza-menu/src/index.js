efa7350718c51d01f5eabcba54c21552aed61b82
marykuo <meiyu19980928@gmail.com>
2025-10-07 22:39:35 +0800

    refactor: extract JSX into a new component

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -65,19 +65,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open from {openHour}:00 to {closeHour}:00. Come visit us or
-            order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
