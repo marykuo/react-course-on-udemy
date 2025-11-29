f04d81ee71506f69bb7c032960e69ea0b7ec2ea6
marykuo <meiyu19980928@gmail.com>
2025-11-29 22:41:18 +0800

    feat: handle event by using state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,19 +1,42 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React",
+  "Build a React App",
+  "Get a beautiful website",
+];
+
 export default function App() {
   const style = { backgroundColor: "#7950f2", color: "#fff" };
+  const [step, setStep] = useState(1);
+
+  async function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  async function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
 
       <div className="buttons">
-        <button style={style}>Previous</button>
-        <button style={style}>Next</button>
+        <button style={style} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={style} onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
