cd23900a60847484c34cf0c864f18f7f7aab2a9e
marykuo <meiyu19980928@gmail.com>
2025-11-29 22:00:56 +0800

    feat: initial steps and message

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,20 @@
 export default function App() {
-  return <div>Hello React!</div>;
+  const style = { backgroundColor: "#7950f2", color: "#fff" };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Hello</p>
+
+      <div className="buttons">
+        <button style={style}>Previous</button>
+        <button style={style}>Next</button>
+      </div>
+    </div>
+  );
 }
