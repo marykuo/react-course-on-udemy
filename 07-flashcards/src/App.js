a2b7d756132b49b3862755c0d196b9d4ed46303e
marykuo <meiyu19980928@gmail.com>
2025-11-30 20:58:48 +0800

    feat: implement flashcards

--- 07-flashcards/src/App.js
+++ 07-flashcards/src/App.js
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import questions from "./data.js";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <FlashCards />
+    </div>
+  );
+}
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id === selectedId ? null : id);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <span>
+            {question.id === selectedId ? question.answer : question.question}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
