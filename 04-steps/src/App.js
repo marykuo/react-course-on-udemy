a674a7a7cdf6d55c9a686b04a1b8360119679ce4
marykuo <meiyu19980928@gmail.com>
2025-11-30 15:33:25 +0800

    fix: use callback function as first argument of setState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -12,16 +12,16 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   async function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   async function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((open) => !open)}>
         &times;
       </button>
 
